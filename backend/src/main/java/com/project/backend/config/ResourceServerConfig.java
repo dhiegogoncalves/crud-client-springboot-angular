efe634c0954ab50b5bd22c470141296d74c3a299
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2020-08-22 20:29:20 -0400

    Implementação do suporta ao docker.

--- backend/src/main/java/com/project/backend/config/ResourceServerConfig.java
+++ backend/src/main/java/com/project/backend/config/ResourceServerConfig.java
@@ -12,7 +12,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/api/usuarios").permitAll()
+        http.authorizeRequests().antMatchers("/api/usuarios", "/status").permitAll()
                 .antMatchers("/api/clientes/**", "/api/servicos-prestados/**").authenticated().anyRequest().denyAll();
     }
 }
